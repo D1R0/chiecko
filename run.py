f74f6ea66b533e6e30bc8836831a0cc39ed987ea
D1R0 <38256732+D1R0@users.noreply.github.com>
2018-10-14 14:56:13 +0300

    Update run.py

--- run.py
+++ run.py
@@ -79,6 +79,7 @@ async def on_message(message):
         await bot.send_message(message.channel, embed=emb)
 
     if message.content == pr+"help":
+        await bot.send_message(message.channel,"Description | Commands \n**Register in Base Data:dividers:** |"+pr+"reg/n**View stats:iphone:**| 'statsus'")
         embed = discord.Embed(title="Description", description="Commands", colour=0x01DFD7)
         embed.add_field(name="**Register in Base Data:dividers:**", value=pr+"reg", inline=True)
         embed.add_field(name="**View stats:iphone:**", value="'status'", inline=True)
