ce378f30045630edfcbe1410bc6c176c078b0c4b
D1R0 <38256732+D1R0@users.noreply.github.com>
2019-01-27 20:50:00 +0200

    Update client.py

--- client.py
+++ client.py
@@ -6,14 +6,7 @@ import platform
 import os
 
 
-bot = commands.Bot(command_prefix='prefix')
-citire= open("prefix.txt","r")
-pr=citire.read()
-citire.close()
-base="data/"
-default=4.66
-
-rgb = ("0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F")
+bot = commands.Bot(command_prefix='>')
 
 @bot.event
 async def on_ready():
