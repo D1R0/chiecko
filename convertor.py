82dd0d90d708998fe8998d42709d3e25af7a9f6d
D1R0 <38256732+D1R0@users.noreply.github.com>
2019-01-27 20:37:59 +0200

    Delete convertor.py

--- convertor.py
+++
@@ -1,8 +0,0 @@
-class convertor(msg):
-        if msg.startswith("lei/euro:") or msg.startswith("euro/lei:"):
-            if msg.startswith("lei/euro:"):
-                content=float(msg.replace("lei/euro:",""))
-                await bot.send_message(message.author,str(round(content/default,1))+" €")
-            else:
-                content=float(msgt.replace("euro/lei:",""))
-                await bot.send_message(message.author,str(round(default*content,1))+" Lei")
